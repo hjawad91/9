a02413e79388d790ab27aade27a7e39d79d34dc7
hasan <hjawad91extended@gmail.com>
2016-07-30 01:48:41 +0600

    3:

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'app1',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
